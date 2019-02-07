094bfc9de42c2d8a67751d9a9dc92f1144280eb7
lange <langejanneck@gmail.com>
2019-02-07 23:20:55 +0100

    add test code for PI

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ const express = require('express'),
     path = require('path'),
     config = require('./config'),
     ErrorREST = require('./errors').ErrorREST,
-    Errors = require('./errors').Errors;
+    Errors = require('./errors').Errors,
+    MongoClient = require('mongodb').MongoClient,
+    Promise = require('promise'),
+    db_url = config.database;
 
 // Routes directory
 const indexRouter = require('./routes/index'),
-    usersRouter = require('./routes/api/users');
+    usersRouter = require('./routes/api/users'),
     authRouter = require('./routes/api/auth');
 
 configureExpress();
-configureDatabase();
+connect(db_url).then(function (db) {
+
+}, function (err) {
+    console.error(err);
+});
 
 module.exports = app;
 
@@ -22,9 +29,9 @@ console.log('Node.js setup finished');
 function configureExpress() {
     app = express();
     app.use(express.json({limit: "8mb"}));
-    app.use(express.urlencoded({ extended: false }));
+    app.use(express.urlencoded({extended: false}));
     app.use(express.static(path.join(__dirname, 'public')));
-    app.use(function(req, res, next) {
+    app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
@@ -32,7 +39,7 @@ function configureExpress() {
     });
 
     // Use body parser so we can get info from POST and/or URL parameters.
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.urlencoded({extended: false}));
     app.use(bodyParser.json({limit: "8mb"}));
 
     // Use morgan to log requests to the console.
@@ -46,11 +53,11 @@ function configureExpress() {
     app.use('/api/auth', authRouter);
 
     // Error handler
-    app.use(function(error, request, response, next) {
+    app.use(function (error, request, response, next) {
         /* TODO Write a log file. */
 
         let validRESTError = error instanceof ErrorREST;
-        if(validRESTError) {
+        if (validRESTError) {
             logRequest(console.log, request);
             console.log("ERROR OCCURRED:");
             console.log(error);
@@ -86,16 +93,29 @@ function configureExpress() {
 
 
 function configureDatabase() {
-    let db_url = config.database;
-
     // Connect to the mongoDB via mongoose
-    mongoose.connect(db_url, { useNewUrlParser: true });
+    mongoose.connect(db_url, {useNewUrlParser: true});
     var db = mongoose.connection;
 
     // Bind connection to error event ( to get notification of connection errors)
     db.on('error', console.error.bind(console, 'connection error: '));
-    db.once('open', function() {
+    db.once('open', function () {
         // connected
         console.log('MongoDB connected..');
+    });
+}
+
+
+function connect(url) {
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(url, function (err, db) {
+            if (err) {
+                console.error('mongo connection error: ', err.message);
+                reject(err);
+            } else {
+                console.info("connected to mongodb");
+                resolve(db);
+            }
+        });
     });
 }
