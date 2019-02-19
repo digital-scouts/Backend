95d853090dbc61df04d7f03d0990ba47aa217592
lange <langejanneck@gmail.com>
2019-02-19 23:09:52 +0100

    add more routes to user

--- controller/userController.js
+++ controller/userController.js
@@ -18,28 +18,24 @@ function getAll(request, response, next) {
 }
 
 /**
- *
+ * add a new user to database
  * @param request
  * @param result
  * @param next
  * @returns {Promise<void>}
  */
 async function addUser(request, result, next) {
-    let user = await modelUser.findOne({email: request.body.email}).lean().exec();
-    if (user) {
+    if (await modelUser.findOne({email: request.body.email}).lean().exec())
         return next(new ErrorREST(Errors.Forbidden, "A user with the provided email already exists"));
-    }
-    let newUser = new modelUser(request.body);
-    newUser.validate(err => {
-        if (err){
-            for (let errName in err.errors) {
-                if(err.errors[errName].name === 'ValidatorError'){
+
+    let user = new modelUser(request.body);
+    user.validate(err => {
+        if (err)
+            for (let errName in err.errors)
+                if(err.errors[errName].name === 'ValidatorError')
                         return next(new ErrorREST(Errors.UnprocessableEntity,err.errors[errName].message))
-                }
-            }
-        }
     });
-    await newUser.save().then(user => result.status(200).json(user)).catch(next);
+    await user.save().then(user => result.status(200).json(user)).catch(next);
 }
 
 /**
@@ -57,9 +53,59 @@ function deleteAll(request, response, next) {
     }
 }
 
+/**
+ * todo set the profile picture
+ * todo check if user has permission
+ * @param request
+ * @param response
+ * @param next
+ */
+function setProfilePicture(request, response, next){
+
+}
+
+/**
+ * get user by id in params
+ * todo permission check
+ * @param request
+ * @param response
+ * @param next
+ */
+function getUser(request, response, next){
+    let requestedUserID = request.params.id;
+    let ownUserID =  request.decoded.userID;
+
+    modelUser.findById(requestedUserID).then(
+        user => {
+            if(user) {
+                response.status(200).json(user)
+            } else {
+                return next(new ErrorREST(Errors.NotFound, "User does not exist."));
+            }
+        }
+    ).catch(next);
+}
+
+/**
+ * delete user by id in params
+ * todo permission check
+ * @param request
+ * @param response
+ * @param next
+ */
+function deleteUser(request, response, next){
+    let requestedUserID = request.params.id;
+    let ownUserID =  request.decoded.userID;
+
+    modelUser.remove({_id: requestedUserID}).then(user => response.json({removedElements: user})).catch(next);
+}
+
 
 module.exports = {
     getAll: getAll,
     addUser: addUser,
-    deleteAll: deleteAll
+    deleteAll: deleteAll,
+    setProfilePicture:setProfilePicture,
+    getUser:getUser,
+    deleteUser:deleteUser
 };
