bb502d21e92839ee94a5a1796ab1c8b84872e25d
LangJa <lange@infoboard.biz>
2019-03-08 09:22:07 +0100

    small things

--- events.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EventEmitter = require('events');
-var myEmitter = new EventEmitter();
-myEmitter.on('newMessage', function (socketId) {
-    console.log('in the middle: an event occurred!');
-});
-exports.default = myEmitter;
--- events.ts
+++ events.ts
@@ -1,7 +1,12 @@
+/**
+ * eventhandler for intern communication
+ */
 const EventEmitter = require('events');
-
 const myEmitter = new EventEmitter();
 
+/**
+ * Man in the middle for debugging
+ */
 myEmitter.on('newMessage', (socketId) => {
     console.log('in the middle: an event occurred!');
 });
