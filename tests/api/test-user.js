ab0cdd78e2b904f8185a972540cff8e16f49861b
LangJa <lange@infoboard.biz>
2019-03-08 17:16:58 +0100

    add unit test for post users

--- tests/api/test-user.js
+++ tests/api/test-user.js
@@ -1,6 +1,8 @@
 const app = require('../../expressApp'),
     server = app.serverE;
 
+const dnOperations = require('../shared/db-operations');
+
 const chai = require('chai'),
     chaiHttp = require('chai-http'),
     should = chai.should(),
@@ -12,41 +14,177 @@ const userModel = require('./../../models/chatModel').Chat;
 
 module.exports = function (test_data) {
     describe('Users', function () {
+        afterEach(function () {
+            dnOperations.clearDatabase();
+        });
 
         describe('/POST user', () => {
             it('should post a new user with minimal correct data', (done) => {
                 chai.request(server)
                     .post('/api/users')
                     .send(test_data.users[0])
-                    .end(async (err, res) => {
-                        test_data.users[0].id = res._id;
-                        console.log("ID: " + res._id)
+                    .end((err, res) => {
                         expect(res).to.have.status(200);
+                        done();
                     });
-                done();
             });
 
-            it.skip('user should exist after post', (done)=>{
-                let dbRequest = userModel.findById(res._id).exec();
-                should.exist(dbRequest);
-                done();
+            it('should post a new user with maximal correct data',(done)=>{
+                chai.request(server)
+                    .post('/api/users')
+                    .send(test_data.users[1])
+                    .end((err, res) => {
+                        expect(res).to.have.status(200);
+                        done();
+                    });
             });
 
-            it('should post a new user with maximal correct data');
+            describe('test wrong emails', ()=>{
+                test_data.wrong_data.email.forEach( (wrong_email) => {
+                    it('should fail with wrong email ('+wrong_email+')', (done) => {
+                         chai.request(server)
+                            .post('/api/users')
+                            .send({//test user with minimal data
+                                name_first: "Test User",
+                                name_last: "Minimal Data",
+                                email: wrong_email,
+                                password: "1234sad??dd.S",
+                                role: "woe",
+                            })
+                            .end((err, res) => {
+                                expect(res).to.have.status(422);
+                                done();
+                            });
+                    });
+                });
+            });
 
-            it('should fail with wrong email');
+            it('should fail without email', (done) =>{
+                chai.request(server)
+                    .post('/api/users')
+                    .send({//test user with minimal data
+                        name_first: "Test User",
+                        name_last: "without email",
+                        password: "1234sad??dd.S",
+                        role: "woe",
+                    })
+                    .end((err, res) => {
+                        expect(res).to.have.status(422);
+                        done();
+                    });
+            });
 
-            it('should fail without email');
+            it('should fail with already existing email', (done)=>{
+                chai.request(server)
+                    .post('/api/users')
+                    .send(test_data.users[1])
+                    .end((err, res) =>{
+                        expect(res).to.have.status(200);
+                        chai.request(server)
+                            .post('/api/users')
+                            .send(test_data.users[1])
+                            .end((err, res) => {
+                                expect(res).to.have.status(403);
+                                done();
+                            });
+                    });
+            });
 
-            it('should fail with already existing email');
+            it('should fail without name_first',(done)=>{
+                chai.request(server)
+                    .post('/api/users')
+                    .send({//test user with minimal data
+                        name_last: "without name_first",
+                        email: "correct@email.de",
+                        password: "1234sad??dd.S",
+                        role: "woe",
+                    })
+                    .end((err, res) => {
+                        expect(res).to.have.status(422);
+                        done();
+                    });
+            });
 
-            it('should fail without name_first');
+            it('should fail without name_last', (done)=>{
+                chai.request(server)
+                    .post('/api/users')
+                    .send({//test user with minimal data
+                        name_first: "without name_last",
+                        email: "correct@email.de",
+                        password: "1234sad??dd.S",
+                        role: "woe",
+                    })
+                    .end((err, res) => {
+                        expect(res).to.have.status(422);
+                        done();
+                    });
+            });
 
-            it('should fail without name_last');
+            it('should fail without role',(done)=>{
+                chai.request(server)
+                    .post('/api/users')
+                    .send({//test user with minimal data
+                        name_first: "test user",
+                        name_last:"without role",
+                        email: "correct@email.de",
+                        password: "1234sad??dd.S",
+                    })
+                    .end((err, res) => {
+                        expect(res).to.have.status(422);
+                        done();
+                    });
+            });
 
-            it('should fail without role');
+            it('should fail with wrong role',(done)=>{
+                chai.request(server)
+                    .post('/api/users')
+                    .send({//test user with minimal data
+                        name_first: "test user",
+                        name_last:"without role",
+                        email: "correct@email.de",
+                        password: "1234sad??dd.S",
+                        role:"bauer"
+                    })
+                    .end((err, res) => {
+                        expect(res).to.have.status(422);
+                        done();
+                    });
+            });
 
-            it('should fail without password');
+            it('should fail without password',(done)=>{
+                chai.request(server)
+                    .post('/api/users')
+                    .send({//test user with minimal data
+                        name_first: "test user",
+                        name_last:"without role",
+                        email: "correct@email.de",
+                        role:"woe"
+                    })
+                    .end((err, res) => {
+                        expect(res).to.have.status(422);
+                        done();
+                    });
+            });
+
+            describe('test unsafe passwords', ()=>{
+                test_data.wrong_data.password.forEach( (password, index) => {
+                    it('should fail with unsafe password ('+password+')', (done) => {
+                        chai.request(server)
+                            .post('/api/users')
+                            .send({//test user with minimal data
+                                name_first: "Test User",
+                                name_last: "Minimal Data",
+                                email: index+"@mail.de",
+                                password: password,
+                                role: "woe",
+                            })
+                            .end((err, res) => {
+                                expect(res).to.have.status(422);
+                                done();
+                            });
+                    });
+                });
+            });
         });
 
         describe('/GET users', () => {
--- tests/shared/test-data.js
+++ tests/shared/test-data.js
@@ -1,10 +1,21 @@
 module.exports = {
-    users: [{
+    users: [{//test user with minimal data
         name_first: "Test User",
         name_last:"Minimal Data",
         email:"user1@mail.de",
         password:"1234sad??dd.S",
         role:"woe",
         id:null
-    }]
+    },{//test user with maximal data
+        name_first: "Test User2",
+        name_last:"Maximal Data",
+        email:"user2@mail.de",
+        password:"1234sad??dd.S",
+        role:"woe",
+        id:null
+    }],
+    wrong_data:{
+        email:["hastPeter","hast@Peter","hastPeter.de"],
+        password:["1234","test","unsafe!","1234567890","LongUnsafePassword!"]
+    }
 };
