d5e0ef9201fdc3fd2cf24d8ef41e61482ae5a33b
LangJa <lange@infoboard.biz>
2019-08-08 14:58:17 +0200

    add timedScript

--- timedScripts.js
+++ timedScripts.js
@@ -8,17 +8,34 @@ var timedScripts = /** @class */ (function () {
     }
     timedScripts.startScripts = function () {
         timedScripts.addRegularTimedGroupLessonEvents();
+        timedScripts.searchForInactiveUsers();
     };
+    /**
+     * Add next upcoming Group Lessons to calendar
+     * At 00:00 on day-of-month 1.
+     */
     timedScripts.addRegularTimedGroupLessonEvents = function () {
         // every 1st day of month
         //todo need to be tested
-        new cron_1.CronJob('0 0 1 * * *', function () {
+        new cron_1.CronJob('0 0 1 * *', function () {
             console.log("monthly Job: add regular timed groupLessonEvents");
             groupLessonModel_1.GroupLesson.find( /* todo find all not finished */).then(function (groupLessons) {
                 groupLessons.forEach(function (groupLesson) {
                     calendarController_1.CalendarController.createNewGroupLessonEvents(groupLesson);
                 });
             });
+        }, function () {
+            console.log('job is done');
+        }, true, 'Europe/Berlin');
+    };
+    /**
+     * Search Inactive Users and update them in DB
+     * At 00:00 on Sunday.
+     */
+    timedScripts.searchForInactiveUsers = function () {
+        new cron_1.CronJob('0 0 * * 0', function () {
+            console.log("weekly Job: search for inactive users");
+            //todo
         }, function () {
             console.log('job is done');
         }, true, 'Europe/Berlin');
--- timedScripts.ts
+++ timedScripts.ts
@@ -6,12 +6,17 @@ class timedScripts {
 
     public static startScripts() {
         timedScripts.addRegularTimedGroupLessonEvents();
+        timedScripts.searchForInactiveUsers();
     }
 
+    /**
+     * Add next upcoming Group Lessons to calendar
+     * At 00:00 on day-of-month 1.
+     */
     private static addRegularTimedGroupLessonEvents() {
         // every 1st day of month
         //todo need to be tested
-        new CronJob('0 0 1 * * *', () => {
+        new CronJob('0 0 1 * *', () => {
             console.log("monthly Job: add regular timed groupLessonEvents");
 
             GroupLesson.find(/* todo find all not finished */).then(groupLessons => {
@@ -19,6 +24,19 @@ class timedScripts {
                     CalendarController.createNewGroupLessonEvents(groupLesson);
                 })
             });
+        }, () => {
+            console.log('job is done')
+        }, true, 'Europe/Berlin');
+    }
+
+    /**
+     * Search Inactive Users and update them in DB
+     * At 00:00 on Sunday.
+     */
+    private static searchForInactiveUsers(){
+        new CronJob('0 0 * * 0', () => {
+            console.log("weekly Job: search for inactive users");
+            //todo
         }, () => {
             console.log('job is done')
         }, true, 'Europe/Berlin');
